bbdb1ec5a23919d96bcad5d50617efdb50beda7f
Андрей Черский <ShaltaiOgurtsov>
2025-07-12 09:01:18 +0300

    Poka

--- Assets/Scripts/ContactGhost.cs
+++ Assets/Scripts/ContactGhost.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class ContactGhost : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Target Settings")]
+    public Transform target;
+
+    [Header("Movement Settings")]
+    [HideInInspector] public float Speed = 5f;
+    [HideInInspector] public float Amplitude = 0.1f;
+    [HideInInspector] public float Frequency = 2f;
+
+    [Header("Damage Settings")]
+    [HideInInspector] public float invulnerabilityDuration = 1.5f;
+
+    private Vector3 velocityPosition;
+    private float floatTimer;
+    private float lastDamageTime = -Mathf.Infinity;
+
     void Start()
     {
-        
+        velocityPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        Vector3 direction = (target.position - velocityPosition).normalized;
+        velocityPosition += direction * Speed * Time.deltaTime;
+        floatTimer += Time.deltaTime;
+        float floatOffset = Mathf.Cos(floatTimer * Frequency) * Amplitude;
+        transform.position = velocityPosition + new Vector3(0f, floatOffset, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+            return;
+
+        if (Time.time - lastDamageTime < invulnerabilityDuration)
+            return;
+
+        if (collision.gameObject.TryGetComponent(out PlayerController controller))
+        {
+            controller.TakeDamege(1);  
+            lastDamageTime = Time.time;
+        }
     }
 }

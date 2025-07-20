ffe4f63213d4eb8005467bcf168300b4246048c3
NicroPoke <nicropokeyt@gmail.com>
2025-07-20 16:24:37 +0300

    kk

--- Assets/Scripts/GhostScript/SkeletonGhost.cs
+++ Assets/Scripts/GhostScript/SkeletonGhost.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 
 public class SkeletonGhost : BaseGhost
 {
-    public Animator skeletonAnimator;
+    private Animator skeletonAnimator;
     [HideInInspector] public bool isRespawning;
     private float respawnCD = 0.5f;
     private float respaenTimer = 0f;
+
+    public AudioSource boneSound;
+
     protected override void Start()
     {
         base.Start();
         skeletonAnimator = GetComponent<Animator>();
         Speed = 2f;
+        invulnerabilityDuration = 1f;
         WanderSpeed = 1.5f;
         GhostType = "Skeleton";
         isPulling = false;
@@ -46,10 +50,15 @@ public class SkeletonGhost : BaseGhost
             rb.constraints = RigidbodyConstraints2D.FreezePosition;
             Alive = false;
 
+            if (boneSound != null)
+            {
+                boneSound.Play();
+            }
+
             StartCoroutine(StartCorpse());
         }
     }
-    
+
     IEnumerator StartCorpse()
     {
         yield return new WaitForSeconds(0.1f);
@@ -58,9 +67,32 @@ public class SkeletonGhost : BaseGhost
         Instantiate(Bullet, transform.position, transform.rotation);
     }
 
-
-    protected override void AnimationCorrector()
+    protected override void OnTriggerStay2D(Collider2D collision)
     {
+        if (!collision.gameObject.CompareTag("Player")) return;
 
+        if (collision.gameObject.TryGetComponent(out PlayerController controller))
+        {
+            if (Time.time - lastDamageTime > invulnerabilityDuration)
+            {
+                controller.TakeDamege(3);
+                lastDamageTime = Time.time;
+
+                isAttacking = true;
+                if (boneSound != null)
+                {
+                    boneSound.Play();
+                }
+                StartCoroutine(ResetAttackFlag());
+            }
+        }
     }
+
+    IEnumerator ResetAttackFlag()
+    {
+        yield return new WaitForSeconds(0.55f);
+        isAttacking = false;
+    }
+
+    protected override void AnimationCorrector() { }
 }
